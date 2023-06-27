455936063292d2e2898930b4bae4d68d65faea43
Vladislav <80133226+Speerekk@users.noreply.github.com>
2023-06-27 22:42:52 +0200

    feature/profile

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -12,15 +12,15 @@ const Profile = ({ username, tag, location, avatar, stats }) => {
       </div>
 
       <ul className={styles.stats}>
-        <li>
+        <li className={styles.border}>
           <span className={styles.label}>Followers</span>
           <span className={styles.quantity}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={styles.border}>
           <span className={styles.label}>Views</span>
           <span className={styles.quantity}>{stats.views}</span>
         </li>
-        <li>
+        <li className={styles.border}>
           <span className={styles.label}>Likes</span>
           <span className={styles.quantity}>{stats.likes}</span>
         </li>
